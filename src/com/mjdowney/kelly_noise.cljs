4d748c71b52462f2b8ab7608b676c89a2c7c73da
Matthew Downey <matthewdowney20@gmail.com>
2023-03-14 12:47:19 -0400

    First version with full simulation responding to controls

--- src/com/mjdowney/kelly_noise.cljs
+++ src/com/mjdowney/kelly_noise.cljs
@@ -23,17 +23,20 @@
 ;;  - Simulate a series of high-probability, low-payoff bets
 ;;  - Simulate a variety of bets
 (ns com.mjdowney.kelly-noise
-  (:require [reagent.dom :as rdom]
-            [reagent.core :as r]
+  (:require [com.mjdowney.kelly.plotly :as plotly]
             [leva.core :as leva]
-            [com.mjdowney.kelly.plotly :as plotly]))
+            [reagent.core :as r]
+            ["seedrandom" :as seedrandom]
+            [reagent.dom :as rdom]
+            [taoensso.encore :as enc]))
 
 ;;; Controls for the simulation
 
 (defonce territory
   (r/atom
-    {:p-win 50
-     :ev 1.1}))
+    {:p-win 0.50
+     :ev 1.1
+     :loss 1.0}))
 
 (defonce simulation
   (r/atom
@@ -54,8 +57,9 @@
    [leva/Controls
     {:folder {:name "Territory" :settings {:order 0}}
      :atom territory
-     :schema {:p-win {:label "P(win)" :min 0 :max 100}
-              :ev {:label "EV" :min 0 :max 2 :step 0.1}}}]
+     :schema {:p-win {:label "P(win)" :min 0 :max 1 :step 0.01}
+              :ev {:label "EV" :min 0 :max 10 :step 0.1}
+              :loss {:label "Loss %" :min 0 :max 1 :step 0.01}}}]
    [leva/Controls
     {:folder {:name "Simulation" :settings {:order 1}}
      :atom simulation
@@ -66,18 +70,64 @@
     {:folder {:name "View" :settings {:order 2 :collapsed true}}
      :atom view}]])
 
+;;; Simulation code
+
+(defn build-bet [{:keys [p-win ev loss]}]
+  (let [p-lose (/ (- 100 (long (* p-win 100))) 100.0)
+        gain (/ (- ev (* p-lose loss)) p-win)]
+    {:p-win p-win
+     :gain  (enc/round* :round 4 gain)
+     :loss loss
+     :p-lose p-lose
+     :ev ev}))
+
+(defn build-bets
+  "Build a series of `n` betting opportunities according to the specified
+  territory."
+  [territory n]
+  (into [] (repeat n (build-bet territory))))
+
+(defn simulate-portfolio
+  "Simulate a portfolio with the given behavior over some series of bets.
+
+  Returns a sequence of bankroll values for each bet."
+  [{:keys [bet-size] :as behavior} bankroll bets random-number-generator]
+  (lazy-seq
+    (when-let [{:keys [p-win gain loss p-lose]} (first bets)]
+      (if (zero? bankroll)
+        (repeat (count bets) 0.0)
+        (let [stake (* bet-size bankroll)
+              outcome (if (< (random-number-generator) p-win)
+                        (+ bankroll (* gain stake))
+                        (- bankroll (* loss stake)))]
+          (cons outcome
+            (simulate-portfolio
+              behavior
+              (if (< outcome 0.0001) 0.0 outcome)
+              (subvec bets 1)
+              random-number-generator)))))))
+
 (defn app []
-  [:div
-   [controls]
-   [plotly/plotly
-    {:data   [{:x      [1 2 3 4]
-               :y      [2 6 3 4]
-               :type   "scatter"
-               :mode   "lines+markers"
-               :marker {:color "red"}}]
-     :layout {:title  "A Fancy Plot"
-              :width  (:width @view)
-              :height (:height @view)}}]])
+  (let [{:keys [portfolios bets] :as sim} @simulation
+        bet-terms (build-bets @territory bets)
+        rng (seedrandom 1)]
+    [:div
+     [controls]
+     [plotly/plotly
+      {:data   (mapv ; simulate 100 portfolios
+                 (fn [_]
+                   {:y          (into []
+                                  (simulate-portfolio sim 1.0 bet-terms rng))
+                    :opacity    0.15
+                    :showlegend false
+                    :type       :scatter})
+                 (range portfolios))
+       :layout {:title  "Portfolios"
+                :xaxis {:title "Bet #"}
+                :yaxis {:title "Bankroll"
+                        :type (if (:y-axis-log @view) "log" "linear")}
+                :width  (:width @view)
+                :height (:height @view)}}]]))
 
 ;;; Lifecycle / entry point
 
